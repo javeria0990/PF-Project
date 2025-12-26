feb3234c05724ef6fc5431be57bafbc02a291fbe
aftabliaqat933@gmail.com <f2025-0961@bnu.edu.pk>
2025-12-25 21:36:58 -0700

    current user Session handling function

--- cpp_backend/auth/main.cpp
+++ cpp_backend/auth/main.cpp
@@ -1,7 +1,10 @@
-#include "auth.h"  
-
+#include "auth.h"
 #include <iostream>
+#include <sstream>
+#include <fstream>
 using namespace std;
+
+int createSession(int uid);
 
 int main(int arguments, char *arg[])
 {
@@ -11,8 +14,55 @@ int main(int arguments, char *arg[])
     }
     else if (arguments == 4)
     {
-        return signIn(arg[2], arg[3]);
+        int result = signIn(arg[2], arg[3]);
+        if (result == 0)
+        {
+            ifstream file;
+            file.open("users.txt");
+            string line;
+            if (file.is_open())
+            {
+                while (getline(file, line))
+                {
+                    int uid;
+                    char separator;
+                    string username, email, password;
+                    stringstream ss(line);
+                    ss >> uid >> separator;
+                    getline(ss, username, '|');
+                    getline(ss, email, '|');
+                    getline(ss, password);
+                    if (email == arg[2])
+                    {
+                        file.close();
+                        return createSession(uid);
+                    }
+                }
+            }
+            else
+            {
+                return -1; // file opening error!
+            }
+        }
+
+        return result;
     }
 
     return 9; // invalid arguments/unexpected behaviour!
+}
+
+int createSession(int uid)
+{
+    ofstream file;
+    file.open("session.txt");
+    if (file.is_open())
+    {
+        file << uid;
+        file.close();
+        return 0; // session created successfully!
+    }
+    else
+    {
+        return -1; // filse opening error!
+    }
 }
